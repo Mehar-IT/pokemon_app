021f2d95fb9e26f19f323d4b5c8cdda02fef675a
Mehar-It <hamzatariquearain@gmail.com>
2022-08-23 20:01:27 +0500

    pokemon complete app

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -10,13 +10,13 @@ export default function Detail(props) {
         <div
             id="popup-modal"
             tabIndex={-1}
-            style={{ backgroundImage: `URL(${poke.img})` }}
-            className="overflow-y-auto pt-11 overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal  justify-center items-center flex bg-no-repeat bg-cover "
+            // style={{ backgroundImage: `URL(${poke.img})` }}
+            className="overflow-y-auto pt-11 overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal  justify-center items-center flex "
             aria-modal="true"
             role="dialog"
         >
             <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 ">
+                <div className="relative bg-yellow-200 rounded-lg shadow dark:bg-gray-700 ">
 
                     <div className="p-6 text-center relative -top-24 m-auto  ">
 
--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -14,11 +14,13 @@ export default function FrontPage() {
   const [poke, setPoke] = useState("")
   const [search, setSearch] = useState("")
   const [page, setPage] = useState(20)
+  const [totalLength, settotalLength] = useState(0)
 
 
   const loadData = async () => {
     const data = await fetchData();
     setTimeout(() => {
+      settotalLength(data.length)
       setPokemone(data.slice(0, page))
       setLoading(true)
       setPage((prev) => prev + 20)
@@ -36,11 +38,10 @@ export default function FrontPage() {
     setShow(value)
     document.body.style.overflow = 'hidden';
   }
-  console.log(page, 'page length');
-  console.log(pokemone.length, 'pokemon length');
+
   return (
     <>
-      {loading ? <div className="bg-white ">
+      {loading ? <div className="bg-yellow-100 h-full">
         <div className="flex items-center w-72 mx-auto pt-10 sticky top-0 ">
           <label htmlFor="simple-search" className="sr-only">
             Search
@@ -79,34 +80,26 @@ export default function FrontPage() {
         <InfiniteScroll
           dataLength={pokemone.length}
           next={loadData}
-          hasMore={page.length != pokemone.length}
+          hasMore={pokemone.length != totalLength}
           loader={<img className="mx-auto p-5" src={spinner} alt="spinner" />}
         >
-          <div className="container mx-auto py-10 px-5  grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-
-
+          <div className=" container mx-auto py-10 px-5  sm:hidden grid grid-cols-2 gap-4">
+            {/* {pokemone.filter(item => item.name.toLowerCase().includes(search)).map((poke, index) => { */}
             {pokemone.map((poke, index) => {
-
-
               if (poke.name.toLowerCase().includes(search) || poke.name.toUpperCase().includes(search)) {
-
                 return (
-                  <div key={index} className=" bg-yellow-200 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
 
+                  <div key={index} className=" bg-yellow-300 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <img
                       className="rounded-t-lg m-auto"
                       src={poke.img}
                       alt="poke image"
                       onClick={() => {
                         setPoke(poke)
-
                         showDetail(true)
-
                       }}
                     />
-
                     <div className="p-5">
-
                       <h5 className="mb-2 text-xl sm:text-2xl sm:font-bold text-center tracking-tight text-gray-900 dark:text-white">
                         {poke.name}
                       </h5>
@@ -115,9 +108,52 @@ export default function FrontPage() {
                 )
               }
             })}
-
-            {show ? <Detail showDetail={showDetail} poke={poke} /> : null}
           </div>
+          <div className="hidden container p-5 mx-auto sm:grid grid-cols-1 xl:grid-cols-2">
+
+            {pokemone.map((poke, index) => {
+              if (poke.name.toLowerCase().includes(search) || poke.name.toUpperCase().includes(search)) {
+                return (
+                  <div key={index} className="flex flex-row bg-yellow-300 rounded-lg border shadow-md dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-2">
+                    <img className="object-contain w-96 ml-5 rounded-t-lg" src={poke.img} alt="" />
+                    <div className="flex flex-col justify-center items-center p-4 space-y-4 w-full">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{poke.name}</h5>
+                      <div className='flex flex-wrap justify-center items-center gap-2 '>
+                        <span className="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">Height:{poke.height && poke.height}</span>
+                        <span className="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">Weight:{poke.weight && poke.weight}</span>
+                        <span className="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">Candy Cound:{poke.candy_count && poke.candy_count}</span>
+                        <span className="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">Egg:{poke.egg && poke.egg}</span>
+                      </div>
+                      <div className='flex justify-center py-3 '>
+                        <span className="bg-gray-100 text-gray-800 text-xs font-semibold mr-2  px-2.5 py-0.5 rounded dark:bg-gray-700  dark:text-gray-300">Type :</span>
+                        {poke.type.map((type, index) => (
+                          <span key={index} className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">{type}</span>
+                        ))}
+
+                      </div>
+                      <span className="bg-gray-100 text-gray-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300 ">weaknesses</span>
+                      <div className='flex justify-center items-center flex-wrap gap-3 my-3 '>
+                        {poke.weaknesses.map((type, index) => (
+                          <span key={index} className="bg-blue-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">{type}</span>
+                        ))}
+                      </div>
+                      <span className="bg-gray-100 text-gray-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300 ">Next Evolution</span>
+                      <div className='flex justify-center items-center flex-wrap gap-3 my-3 '>
+                        {poke.next_evolution && poke.next_evolution.map((type, index) => (
+                          <div key={index}>
+                            <span className="bg-blue-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800" > {type.num}</span>
+                            <span className="bg-blue-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">{type.name}</span>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                )
+              }
+            })}
+          </div>
+
+          {show ? <Detail showDetail={showDetail} poke={poke} /> : null}
         </InfiniteScroll>
       </div > : <div className="h-screen w-screen bg-yellow-300 flex justify-center items-center"><img className="App-logo" src={pokemoneLogo} alt="poke splash" /></div>}
     </>
